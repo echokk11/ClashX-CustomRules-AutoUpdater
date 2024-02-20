22f7c9bb804d3fce98e76a2255cbe4814fb4a470
Ice Easy <ice@easy>
2024-02-21 00:59:09 +0800

    calories

--- src/main/java/io/echokk11/clashxcustomrulesautoupdater/calories/Calc.java
+++ src/main/java/io/echokk11/clashxcustomrulesautoupdater/calories/Calc.java
@@ -2,6 +2,9 @@ package io.echokk11.clashxcustomrulesautoupdater.calories;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Data
 public class Calc {
 
@@ -23,15 +26,16 @@ public class Calc {
     }
 
     public void run() {
+        System.out.printf("当前体重为[%s]kg%n", BigDecimal.valueOf(weight).setScale(1, RoundingMode.FLOOR));
         int c = Double.valueOf(weight * PER_CARBOHYDRATE).intValue();
         int p = Double.valueOf(weight * PER_PROTEIN).intValue();
         int f = Double.valueOf(weight * PER_FAT).intValue();
-        System.out.printf("每天需要摄入%dg碳水，%dg蛋白质，%dg脂肪%n", c, p, f);
+        System.out.printf("每天需要摄入[%d]g碳水，[%d]g蛋白质，[%d]g脂肪%n", c, p, f);
         int per_day_kcal = c * KCAL_CARBOHYDRATE + p * KCAL_PROTEIN + f * KCAL_FAT;
-        System.out.printf("每天的热量摄入约为%d大卡%n", per_day_kcal);
+        System.out.printf("每天的热量摄入约为[%d]大卡%n", per_day_kcal);
     }
 
     public static void main(String[] args) {
-        new Calc(85.0).run();
+        new Calc(82.0D).run();
     }
 }
