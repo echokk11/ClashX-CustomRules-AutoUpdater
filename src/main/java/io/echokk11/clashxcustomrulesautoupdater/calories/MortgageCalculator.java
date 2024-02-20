7a5f30ebff453d2ef01fef6b8d813980690fdfb7
Ice Easy <ice@easy>
2024-02-21 01:37:20 +0800

    calories

---
+++ src/main/java/io/echokk11/clashxcustomrulesautoupdater/calories/MortgageCalculator.java
@@ -0,0 +1,31 @@
+package io.echokk11.clashxcustomrulesautoupdater.calories;
+
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class MortgageCalculator {
+
+    private static double fee = 2770100.73;  //贷款总额
+    private static double rate = 0.042D; //贷款利率
+//  private static double rate = 0.0489D; //贷款利率
+
+    private static int totalMonth = 360; //贷款期限
+    private static int payMonth = 49; //已还期数
+    private static int month = totalMonth - payMonth; //剩余期数
+    public static void main(String[] args) {
+        double monthRate = rate / 12;
+        double monthPay = fee * monthRate * Math.pow(1 + monthRate, month) / (Math.pow(1 + monthRate, month) - 1);
+        System.out.println("每月还款额：" + toScaledBigDecimal(monthPay));
+        double total = monthPay * month;
+        System.out.println("还款总额：" + toScaledBigDecimal(total));
+        double totalInterest = total - fee;
+        System.out.println("总利息：" + toScaledBigDecimal(totalInterest));
+        double totalInterestRate = totalInterest / fee;
+        System.out.println("总利率：" + toScaledBigDecimal(totalInterestRate).multiply(BigDecimal.valueOf(100)) + "%");
+    }
+
+    static BigDecimal toScaledBigDecimal(double in) {
+        return BigDecimal.valueOf(in).setScale(2, RoundingMode.FLOOR);
+    }
+}
