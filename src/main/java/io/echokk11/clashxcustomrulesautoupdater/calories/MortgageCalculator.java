f285fc82364ec38399b45bd2d1fa3db5c7d92de1
watchman <watchman@matrix.com>
2024-03-04 12:12:03 +0800

    1

--- src/main/java/io/echokk11/clashxcustomrulesautoupdater/calories/MortgageCalculator.java
+++ src/main/java/io/echokk11/clashxcustomrulesautoupdater/calories/MortgageCalculator.java
@@ -6,7 +6,8 @@ import java.math.RoundingMode;
 
 public class MortgageCalculator {
 
-    private static double fee = 2770100.73;  //贷款总额
+    private static double fee = 2770068.43;  //贷款总额
+    private static double pre_fee = 800000.0;  //提前还款总额
     private static double rate = 0.042D; //贷款利率
 //  private static double rate = 0.0489D; //贷款利率
 
@@ -14,15 +15,19 @@ public class MortgageCalculator {
     private static int payMonth = 49; //已还期数
     private static int month = totalMonth - payMonth; //剩余期数
     public static void main(String[] args) {
+        System.out.println("剩余本金：" + toScaledBigDecimal(fee));
+        System.out.println("提前还款金额：" + toScaledBigDecimal(pre_fee));
+        fee = fee - pre_fee;
+        System.out.println("剩余还款本金：" + toScaledBigDecimal(fee));
         double monthRate = rate / 12;
         double monthPay = fee * monthRate * Math.pow(1 + monthRate, month) / (Math.pow(1 + monthRate, month) - 1);
-        System.out.println("每月还款额：" + toScaledBigDecimal(monthPay));
         double total = monthPay * month;
         System.out.println("还款总额：" + toScaledBigDecimal(total));
         double totalInterest = total - fee;
         System.out.println("总利息：" + toScaledBigDecimal(totalInterest));
         double totalInterestRate = totalInterest / fee;
         System.out.println("总利率：" + toScaledBigDecimal(totalInterestRate).multiply(BigDecimal.valueOf(100)) + "%");
+        System.out.println("每月还款额：" + toScaledBigDecimal(monthPay));
     }
 
     static BigDecimal toScaledBigDecimal(double in) {
